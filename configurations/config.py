94734df672d94a62a50b57335185ffda3d5d6ba2
Aditya <21mc3003@rgipt.ac.in>
2025-04-20 16:24:12 +0530

    config update

--- configurations/config.py
+++ configurations/config.py
@@ -1,6 +1,9 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 client = MongoClient(os.getenv('MONGO_URI'), server_api=ServerApi('1')) 
 
@@ -15,27 +18,46 @@ def get_db():
 from pydantic_settings import BaseSettings
 from pydantic import EmailStr
 
+# class Settings(BaseSettings):
+#     MONGO_URI: str
+#     JWT_SECRET: str
+#     JWT_ALGORITHM: str
+#     EMAIL_SENDER: EmailStr
+#     SMTP_SERVER: str
+#     SMTP_PORT: int
+#     SMTP_USERNAME: str
+#     SMTP_PASSWORD: str
+#     ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+#         # Cloudinary Configuration
+#     CLOUDINARY_CLOUD_NAME: str
+#     CLOUDINARY_API_KEY: str
+#     CLOUDINARY_API_SECRET: str
+
+#     REDIS_URL: str
+#     BASE_URL: str = os.getenv("FEEDBACK_BASE_URL", "http://localhost:8000")
+
+#     class Config:
+#         env_file = ".env"
+
+
 class Settings(BaseSettings):
-    MONGO_URI: str
-    JWT_SECRET: str
-    JWT_ALGORITHM: str
-    EMAIL_SENDER: EmailStr
-    SMTP_SERVER: str
-    SMTP_PORT: int
-    SMTP_USERNAME: str
-    SMTP_PASSWORD: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    MONGO_URI: str = os.getenv("MONGO_URI")
+    JWT_SECRET: str = os.getenv("JWT_SECRET")
+    JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM")
+    EMAIL_SENDER: EmailStr = os.getenv("EMAIL_SENDER")
+    SMTP_SERVER: str = os.getenv("SMTP_SERVER")
+    SMTP_PORT: int = int(os.getenv("SMTP_PORT", 587))
+    SMTP_USERNAME: str = os.getenv("SMTP_USERNAME")
+    SMTP_PASSWORD: str = os.getenv("SMTP_PASSWORD")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
-        # Cloudinary Configuration
-    CLOUDINARY_CLOUD_NAME: str
-    CLOUDINARY_API_KEY: str
-    CLOUDINARY_API_SECRET: str
+    CLOUDINARY_CLOUD_NAME: str = os.getenv("CLOUDINARY_CLOUD_NAME")
+    CLOUDINARY_API_KEY: str = os.getenv("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET: str = os.getenv("CLOUDINARY_API_SECRET")
 
-    REDIS_URL: str
+    REDIS_URL: str = os.getenv("REDIS_URL")
     BASE_URL: str = os.getenv("FEEDBACK_BASE_URL", "http://localhost:8000")
-
-    class Config:
-        env_file = ".env"
 
 settings = Settings()
 
