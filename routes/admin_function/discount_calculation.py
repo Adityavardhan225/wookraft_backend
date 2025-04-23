1b51059d7934b338fddc6a857852f2bebd458e58
Aditya <21mc3003@rgipt.ac.in>
2025-04-23 14:34:50 +0530

    render.yaml fseffsd

--- routes/admin_function/discount_calculation.py
+++ routes/admin_function/discount_calculation.py
@@ -367,6 +367,14 @@ celery_app = Celery(
     broker=REDIS_URL,
     backend=REDIS_URL
 )
+import ssl
+if REDIS_URL.startswith("rediss://"):
+    celery_app.conf.broker_use_ssl = {
+        "ssl_cert_reqs": ssl.CERT_NONE  # Disable certificate validation
+    }
+    celery_app.conf.redis_backend_use_ssl = {
+        "ssl_cert_reqs": ssl.CERT_NONE  # Disable certificate validation
+    }
 
 # Celery configuration
 celery_app.conf.update(
